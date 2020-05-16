de3dffdf127bbcff103d91e338de740dd5d136f0
dkos <kosdejan@yahoo.com>
2020-05-16 12:29:10 +0200

    7_1 fmt

--- src/c7_q1.rs
+++ src/c7_q1.rs
@@ -1,10 +1,5 @@
 // Deck of Cards: Design the data structures for a generic deck of cards. Explain how you would
 // subclass the data structures to implement blackjack.
-
-
-
-
-
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 
